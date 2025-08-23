d7f8e73f8e360bb1afb299478456f44da303a29b
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-23 00:46:17 -0300

    Removed Discord stuff (for now)

--- src/discord/discord.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DiscordService } from './discord.service';
-import { DiscordController } from './discord.controller';
-
-@Module({
-  controllers: [DiscordController],
-  providers: [DiscordService],
-})
-export class DiscordModule {}
--- src/discord/dto/update-discord.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDiscordDto } from './create-discord.dto';
-
-export class UpdateDiscordDto extends PartialType(CreateDiscordDto) {}
