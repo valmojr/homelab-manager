9e52deee7807fdabd000ba5b9870fc50ac198817
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-24 01:06:52 -0300

    Added root module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DiscordModule } from './discord/discord.module';
 import { TelegramModule } from './telegram/telegram.module';
-import { UtilsModule } from './utils/utils.module';
+import { WeatherModule } from './weather/weather.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import TasksService from './tasks.service';
 
 @Module({
-  imports: [DiscordModule, TelegramModule, UtilsModule],
+  imports: [ ScheduleModule.forRoot(),
+  TelegramModule, WeatherModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
+
 export class AppModule {}
