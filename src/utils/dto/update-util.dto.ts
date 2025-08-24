2d16a04e9303f403b8ceca534bff3fb091fb4183
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-24 01:05:51 -0300

    Removed utils as a single entity

--- src/utils/dto/update-util.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUtilDto } from './create-util.dto';
-
-export class UpdateUtilDto extends PartialType(CreateUtilDto) {}
--- src/utils/utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UtilsService } from './utils.service';
-import { UtilsController } from './utils.controller';
-
-@Module({
-  controllers: [UtilsController],
-  providers: [UtilsService],
-})
-export class UtilsModule {}
