7d8c1c1a0d9e15ddd6e43c022a92169d5d7c2a42
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-24 11:12:29 -0300

    added scaffoded weather entity

---
+++ src/weather/weather.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+
+@Controller('weather')
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+}
