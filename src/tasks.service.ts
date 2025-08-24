77a335f10f87f126d7c5ac570332a0decfbf4f2d
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-24 19:50:05 -0300

    Added dev ver cron jobs

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { Interval } from "@nestjs/schedule";
 import { TelegramService } from "./telegram/telegram.service";
+import { ReportService } from "./report/report.service";
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly telegramService: TelegramService) {
+  constructor(private readonly telegramService: TelegramService, private readonly reportService: ReportService) {    
     this.sendReport();
   }
 
   async sendReport() {
-    return await this.telegramService.sendMessage(process.env.TELEGRAM_CHAT_ID || "", "This is a periodic report message.");
+    const report = await this.reportService.generateReport();
+    return await this.telegramService.sendMessage(process.env.TELEGRAM_CHAT_ID || "", report);
   }
 }
