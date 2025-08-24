404b0f46f44aa181b09cf682baafd6f548d286d3
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-24 13:24:54 -0300

    added dev ver to send report when app starts

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -4,9 +4,10 @@ import { TelegramService } from "./telegram/telegram.service";
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly telegramService: TelegramService) {}
+  constructor(private readonly telegramService: TelegramService) {
+    this.sendReport();
+  }
 
-  @Interval(10000)
   async sendReport() {
     return await this.telegramService.sendMessage(process.env.TELEGRAM_CHAT_ID || "", "This is a periodic report message.");
   }
