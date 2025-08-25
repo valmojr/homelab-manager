abd11bb725a45bfec9cff3254bcf27d997059bb2
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-25 02:45:53 -0300

    Added cron job at 7am and made a handler for prod/dev envs

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from "@nestjs/common";
-import { Interval } from "@nestjs/schedule";
+import { Injectable, Logger } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 import { TelegramService } from "./telegram/telegram.service";
 import { ReportService } from "./report/report.service";
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly telegramService: TelegramService, private readonly reportService: ReportService) {    
-    this.sendReport();
+  constructor(private readonly telegramService: TelegramService, private readonly reportService: ReportService) {
+    process.env.NODE_ENV !== 'production' && this.sendReport();
   }
 
+  @Cron('0 7 * * *')
   async sendReport() {
     const report = await this.reportService.generateReport();
     return await this.telegramService.sendMessage(process.env.TELEGRAM_CHAT_ID || "", report);
