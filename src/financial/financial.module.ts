4ef2cf26048b2efab23ce83c96d2f188210b844e
Valmo Trindade <val_mo_jr@hotmail.com>
2025-08-26 13:16:51 -0300

    Fixed imports on financial module

--- src/financial/financial.module.ts
+++ src/financial/financial.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FinancialService } from './financial.service';
-import { FinancialController } from './financial.controller';
 
 @Module({
-  controllers: [FinancialController],
   providers: [FinancialService],
 })
 export class FinancialModule {}
